89ee8a205ddc26ecd9f313cbbe43dcaf1a7eb3d7
garronej <joseph.garrone@protonmail.com>
2025-05-30 19:23:33 +0200

    No need to include account-v1 in the NPM bundle

--- scripts/build.overridable.ts
+++ scripts/build.overridable.ts
@@ -1,10 +1,1 @@
-import { getThisCodebaseRootDirPath } from "./tools/getThisCodebaseRootDirPath.overridable";
-import { transformCodebase } from "../node_modules/keycloakify/src/bin/tools/transformCodebase";
-import { join as pathJoin } from "path";
-
-export async function extraSteps() {
-    transformCodebase({
-        srcDirPath: pathJoin(getThisCodebaseRootDirPath(), "account-v1"),
-        destDirPath: pathJoin(getThisCodebaseRootDirPath(), "dist", "account-v1")
-    });
-}
+export async function extraSteps() {}
